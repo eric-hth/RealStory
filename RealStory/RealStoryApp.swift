25a05bbcc9cc7a05a54a89496c1a036d58eb7e60
eric_hth <eric_hong_2000@yahoo.fr>
2025-04-28 13:07:37 +0200

    Initial Commit

---
+++ RealStory/RealStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealStoryApp.swift
+//  RealStory
+//
+//  Created by Eric Hong Tuan Ha on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RealStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
